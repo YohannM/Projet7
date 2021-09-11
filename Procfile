654e4fbebc9f489424c714e979f93146c34269f7
YohannM <yohannmarmonier@gmail.com>
2021-09-11 19:03:28 +0100

    Removed conf files from lfs

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8f205f240718632b3061c49205f046025426e6d7c70d01430eb5f6cdd136f9b4
-size 40
+web: sh setup.sh && streamlit run dashboard.py
--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:35fe443bf459259c938366baeafc5d26726c1d5234212ec02aa3b4cd13fbd514
-size 132
+mkdir -p ~/.streamlit/
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
